10a33dd3b657d40ace496495161e1144808129e8
navneet9122 <66055030+navneet9122@users.noreply.github.com>
2023-06-14 15:37:16 +0530

    Add files via upload

---
+++ python/call-modern.py
@@ -0,0 +1,6 @@
+import modern
+#print(dir(modern))
+
+modern.order_food("Salad", "Pizza", "Biryani", "Soup")
+modern.vac_feedback(efficacy=34, vac="Unknown")
+modern.time_activity(10, 20, 10, hobbby="Dance", sport="Boxing", fun="Driving", work="DevOps")
